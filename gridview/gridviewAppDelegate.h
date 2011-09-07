79d2dadbfc99a6340f69523ff9dc0f580732420d
echoz <jeremyfoo@gmail.com>
2011-09-07 09:50:23 +0800

    Initial Commit

---
+++ gridview/gridviewAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  gridviewAppDelegate.h
+//  gridview
+//
+//  Created by Jeremy Foo on 9/7/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface gridviewAppDelegate : NSObject <UIApplicationDelegate>
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
